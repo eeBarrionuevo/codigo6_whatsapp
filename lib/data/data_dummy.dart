10c6886d6d64b5f6512745634ad410363f1f2287
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2023-01-24 21:24:31 -0500

    Lista de chats

--- lib/data/data_dummy.dart
+++ lib/data/data_dummy.dart
@@ -3,25 +3,36 @@ import 'package:codigo6_whatsapp/models/chat_model.dart';
 class DataDummy {
   List<ChatModel> chats = [
     ChatModel(
-      avatar: "sdsdsd",
-      name: "asdsad",
-      message: "asdsad",
+      avatar:
+          "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      name: "He llegado un poco tarde",
+      message: "Juan Ramos Torres",
       time: "123",
       date: "1231/1222",
     ),
     ChatModel(
-      avatar: "sdsdsd",
-      name: "asdsad",
-      message: "asdsad",
-      time: "123",
-      date: "1231/1222",
+      avatar:
+          "https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      name: "Fiorella Ramos Torres",
+      message: "Hola, enviame el link de la clase",
+      time: "12:40",
+      date: "12/12",
     ),
     ChatModel(
-      avatar: "sdsdsd",
-      name: "asdsad",
-      message: "asdsad",
-      time: "123",
-      date: "1231/1222",
+      avatar:
+          "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      name: "Maria Montes Lopez",
+      message: "Ya he enviado todos los trabajos",
+      time: "11:50",
+      date: "04/10",
+    ),
+    ChatModel(
+      avatar:
+          "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      name: "Susana Zapata Ruiz",
+      message: "Dónde estas? Por favor no tardes mucho",
+      time: "07:05",
+      date: "12:07",
     ),
     // {
     //   "avatar":
